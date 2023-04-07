2be74bdb7ac75dee0e759a80387e5dba96b315d9
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-04-07 23:17:05 +0530

    COntact route added & Some random changes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
+
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='h-screen flex flex-col justify-center items-center gap-10 pt-5 bg-gradient-to-r from-yellow-200 via-yellow-400 to-yellow-700'>
+      <a href='https://www.linkedin.com/in/kaushik-gabhane-013708203/' target='_blank'  rel="noopener noreferrer" className='flex gap-5 items-center'>
+        <i className="fa-brands fa-linkedin fa-shake fa-2xl" style={{color: "#3a71df"}}></i> 
+        <span className='font-mono font-semibold text-lg'>Linkedin</span>
+      </a>
+      <a href='https://github.com/kaushikgabhane' target='_blank'  rel="noopener noreferrer" className='flex gap-5 items-center'>
+        <i className="fa-brands fa-github fa-shake fa-2xl" style={{color: "#000000"}}></i>
+        <span className='font-mono font-semibold text-lg'>Github</span> 
+      </a>
+      <a href='https://twitter.com/KaushikGabhane' target='_blank'  rel="noopener noreferrer" className='flex gap-5 items-center'>
+        <i className="fa-brands fa-twitter fa-shake fa-2xl" style={{color: "#2f6eda"}}></i>
+        <span className='font-mono font-semibold text-lg'>Twitter</span>  
+      </a>
+      <a href='https://kaushik543.hashnode.dev/' target='_blank'  rel="noopener noreferrer" className='flex gap-5 items-center'>
+        <i className="fa-solid fa-circle-user fa-shake fa-2xl" style={{color: "#3DBE29"}}></i>
+         <span className='font-mono font-semibold text-lg'>Blogs</span>  
+      </a>
+    </div>
   )
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
 
 
 const Header = () => {
+
+  const [nav,setNav] = useState("visible");
+
+  const handleNav = () => {
+    if (nav === "visible") {
+      return setNav("hidden");
+    }
+    if(nav !== "visible"){
+      return setNav("visible");
+    }
+  }
+
+
     return (
       <header className="header w-full flex flex-row bg-slate-100 justify-between p-3 px-10 shadow-lg shadow-[#333] fixed top-0 z-10">
           <h3 className="logo font-serif font-bold text-2xl text-fuchsia-600 animate-bounce	">🍔Food Corner🍔</h3>
-         
-          <ul className='navList flex list-none gap-10'>
+          <i className="fa-solid fa-bars sm:hidden" style={{color: "#3de686"}} onClick={() => handleNav }></i>
+          <ul className='navList md:flex md:list-none md:gap-10' style={{visibility:{nav}}}>
               <Link to="/">
                 <li className="font-bold text-lg text-blue-500">Home</li>
               </Link>
